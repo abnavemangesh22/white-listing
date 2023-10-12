94fa0e6a5dbf4a263e9620e056d9f4506eceeca3
abnavemangesh22 <69311384+abnavemangesh22@users.noreply.github.com>
2023-10-12 18:45:53 +0530

    Update gencerts.sh

--- gencerts.sh
+++ gencerts.sh
@@ -13,3 +13,17 @@ chmod 644 *.key
 cp ca.crt ./app
 cp server.crt ./app
 cp server.key ./app
+
+=============================================================================================================================
+
+Refer the Link:
+https://stackoverflow.com/questions/64814173/how-do-i-use-sans-with-openssl-instead-of-common-name
+
+CA creation steps
+# openssl genrsa -out ca.key 2048
+# openssl req -new -x509 -days 365 -key ca.key -subj "/C=CN/ST=GD/L=SZ/O=Acme, Inc./CN=Acme Root CA" -out ca.crt
+
+Server key and Server Cert creation:
+
+# openssl req -newkey rsa:2048 -nodes -keyout server.key -subj "/C=CN/ST=GD/L=SZ/O=Acme, Inc./CN=whitelist-registry.default.svc" -out server.csr
+# openssl x509 -req -extfile <(printf "subjectAltName=DNS:whitelist-registry.default.svc") -days 365 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
